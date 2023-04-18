4911189deae985469888558b75c874b0911a1431
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-18 18:38:25 +0530

    ADDED:code to print

---
+++ Generic.java
@@ -0,0 +1,26 @@
+package com.day13problem;
+
+public class Generic {
+
+    public void Integer(Integer a, Integer b, Integer c) {
+        if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+            System.out.println("a is greater " + a);
+        } else if (b.compareTo(c) > 0) {
+
+            System.out.println("b is greater " + b);
+        } else {
+            System.out.println("c is greater ");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Generic gen = new Generic();
+        gen.Integer(67,89, 78);
+    }
+}
+
+
+
+
