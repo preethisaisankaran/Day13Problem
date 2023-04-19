0d631a4715aa808446da524933638b32b7abe091
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-19 22:04:01 +0530

    ADDED:code to print in generic method

--- Generic.java
+++ Generic.java
@@ -1,25 +1,29 @@
 package com.day13problem;
 
 public class Generic{
-    public void String(String a, String b, String c) {
-      if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-          System.out.println("a is greater " + a);
-        } else if (b.compareTo(c) > 0) {
+    public static <E extends Comparable> void printArray(E a, E b, E c) {
+        E max = a;
 
-          System.out.println("b is greater " + b);
-      } else {
-          System.out.println("c is greater ");
+        if (b.compareTo(max) > 0) {
+            max = b;
 
-     }
+        }
+       if (c.compareTo(max) > 0) {
+           max = c;
+       }
+       System.out.println(max);
+   }
 
 
-    }
 
 
 
     public static void main(String[] args) {
         Generic gen = new Generic();
-        gen.String("Apple","Peach","Banana");
+        gen.printArray(100, 500, 700);
+        gen.printArray(3.5f, 6.8f, 2.5f);
+        gen.printArray("Apple", "Peach", "Banana");
+
 
 
 
