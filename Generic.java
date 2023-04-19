9bfa8428fa652267cdff0fe9a0224d570f20e25b
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-19 21:58:50 +0530

    ADDED:code to check max string

--- Generic.java
+++ Generic.java
@@ -1,7 +1,7 @@
 package com.day13problem;
 
 public class Generic{
-    public void Float(Float a, Float b, Float c) {
+    public void String(String a, String b, String c) {
       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
           System.out.println("a is greater " + a);
         } else if (b.compareTo(c) > 0) {
@@ -19,7 +19,7 @@ public class Generic{
 
     public static void main(String[] args) {
         Generic gen = new Generic();
-        gen.Float(4.5f,2.5f,3.8f);
+        gen.String("Apple","Peach","Banana");
 
 
 
