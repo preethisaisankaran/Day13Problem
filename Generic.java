3ccb001ae0c088b9e0fb5b215e13634ae9d4583b
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-19 21:54:29 +0530

    ADDED:code find the max float

--- Generic.java
+++ Generic.java
@@ -1,23 +1,25 @@
 package com.day13problem;
 
 public class Generic{
+    public void Float(Float a, Float b, Float c) {
+      if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
+          System.out.println("a is greater " + a);
+        } else if (b.compareTo(c) > 0) {
 
-   public void Integer(Integer a, Integer b, Integer c) {
-       if (a.compareTo(b) > 0 && a.compareTo(c) > 0) {
-           System.out.println("a is greater " + a);
-       } else if (b.compareTo(c) > 0) {
+          System.out.println("b is greater " + b);
+      } else {
+          System.out.println("c is greater ");
 
-           System.out.println("b is greater " + b);
-       } else {
-           System.out.println("c is greater " +c);
+     }
 
-      }
-   }
+
+    }
+
 
 
     public static void main(String[] args) {
         Generic gen = new Generic();
-        gen.Integer(30,67,200);
+        gen.Float(4.5f,2.5f,3.8f);
 
 
 
